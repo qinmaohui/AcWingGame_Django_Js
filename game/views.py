7a10ae2c51fa235377da7e404145db8f49e600ef
qmh <820102443@qq.com>
2022-01-25 13:06:58 +0800

    creat project structure

--- game/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    line1='<h1 style="text-align:center">术士之战</h1>'
-    return HttpResponse(line1)
-def play(request):
-    line1='<h1 style="text-align:center">游戏界面</h1>'
-    return HttpResponse(line1)
