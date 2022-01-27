0d4feae5fcc6be007055569485973723e7fc0346
qmh <820102443@qq.com>
2022-01-27 19:07:38 +0800

    create single mode 1.0

--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -1,10 +1,25 @@
 class AcGamePlayground{
     constructor(root){
         this.root=root;
-        this.$playground= $(`<div>游戏界面</div>`);
-        this.hide();
+        this.$playground= $(`<div class="ac-game-playground"></div>`);
+        //this.hide();
         this.root.$ac_game.append(this.$playground);
+        this.width=this.$playground.width();
+        this.height=this.$playground.height();
+        this.game_map=new GameMap(this);
+        this.players=[];
+        this.players.push(new Player(this,this.width/2,this.height/2,this.height*0.05,"white",this.height*0.15, true));
+        
+        for(let i=0;i<5;i++){
+            this.players.push(new Player(this,this.width/2,this.height/2,this.height*0.05,this.get_random_color(),this.height*0.15,false));
+        }
+
         this.start();
+    }
+    
+    get_random_color(){
+        let colors=["blue","red","yellow","pink","grey","green","#faa755","#9b95c9"];
+        return colors[Math.floor(Math.random()*colors.length)];
     }
 
     start(){
