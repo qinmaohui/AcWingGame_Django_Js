b8455d99fb78142eb662f8253cc8be651937067b
qmh <820102443@qq.com>
2022-01-28 15:01:39 +0800

    create a AcApp on AcWing

--- acapp/settings.py
+++ acapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wi2o1wb%qa0h9)p-gjz)*gswh(5(r2^pntp()5uw1^b)a9@u05'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["119.23.142.47"]
+ALLOWED_HOSTS = ["119.23.142.47","app1327.acapp.acwing.com.cn"]
 
 
 # Application definition
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
