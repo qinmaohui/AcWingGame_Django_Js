8c63700c39fcbf014ebbb7a6e987b166e1419532
qmh <820102443@qq.com>
2022-02-15 12:40:44 +0800

    finish login system 1.0

--- static/js/src/settings/zbase.js
+++ static/js/src/settings/zbase.js
@@ -48,12 +48,12 @@ class Settings{
                 <input type="text" placeholder="用户名">
             </div>
         </div>
-        <div class="ac-game-settings-password ac-game-settings-first">
+        <div class="ac-game-settings-password ac-game-settings-password-first">
             <div class="ac-game-settings-item">
                 <input type="password" placeholder="密码">
             </div>
         </div>
-        <div class="ac-game-settings-password ac-game-settings-second">
+        <div class="ac-game-settings-password ac-game-settings-password-second">
             <div class="ac-game-settings-item">
                 <input type="password" placeholder="确认密码">
             </div>
@@ -82,7 +82,7 @@ class Settings{
         this.$login_username=this.$login.find(".ac-game-settings-username input");
         this.$login_password=this.$login.find(".ac-game-settings-password input");4
         this.$login_submit=this.$login.find(".ac-game-settings-submit button");
-        this.$login_error_message=this.$login.find(".ac-game-settings-error-messages");
+        this.$login_error_message=this.$login.find(".ac-game-settings-error-message");
         this.$login_register=this.$login.find(".ac-game-settings-option");
         this.$login.hide();
 
@@ -112,14 +112,81 @@ class Settings{
         this.$login_register.click(function () {
             outer.register();
         });
+        this.$login_submit.click(function () {
+            outer.login_on_remote();
+        });
     }
     add_listening_events_register(){
         let outer=this;
         this.$register_login.click(function () {
             outer.login();
         });
+        this.$register_submit.click(function () {
+           outer.register_on_remote();
+        });
     }
+    login_on_remote(){//登录远程服务器
+        let outer=this;
+        let username=this.$login_username.val();
+        let password=this.$login_password.val();
+        this.$login_error_message.empty();
+        $.ajax({
+            url:"https://app1327.acapp.acwing.com.cn/settings/login/",
+            type:"GET",
+            data:{
+                username:username,
+                password:password,
+            },
+            success:function (resp){
+                console.log(resp);
+                if (resp.result==="success"){
+                    location.reload();
+                }else{
+                    outer.$login_error_message.html(resp.result);
+                }
+            }
+        });
+    }
+    register_on_remote(){//在远程服务器上注册
+        let outer=this;
+        let username=this.$register_username.val();
+        let password=this.$register_password.val();
+        let password_confirm=this.$register_password_confirm.val();
+        this.$register_error_message.empty();
+        console.log(password);
 
+        $.ajax({
+            url:"https://app1327.acapp.acwing.com.cn/settings/register/",
+            type:"GET",
+            data:{
+                username:username,
+                password:password,
+                password_confirm:password_confirm,
+            },
+            success:function (resp){
+                console.log(resp);
+                if(resp.result==="success"){
+                    location.reload();//刷新页面
+                }else{
+                    outer.$register_error_message.html(resp.result);
+                }
+            }
+
+        });
+    }
+    logout_on_remote(){//在远程服务器上登出
+        if(this.platform==="ACAPP")return false;
+        $.ajax({
+            url:"https://app1327.acapp.acwing.com.cn/settings/logout/",
+            type:"GET",
+            success:function (resp) {
+                console.log(resp);
+                if(resp.result==="success"){
+                    location.reload();
+                }
+            }
+        });
+    }
     register(){//打开注册界面
         this.$login.hide();
         this.$register.show();
